5c55feb9b28ec22106ae265c210c859dc0f5e9c4
Maitry Sinha <maitry@google.com>
2025-12-30 14:36:17 +0530

    feat: Add modernbert tokenizer

--- keras_hub/src/models/modernbert/modernbert_tokenizer.py
+++ keras_hub/src/models/modernbert/modernbert_tokenizer.py
@@ -2,6 +2,8 @@ import keras
 from keras_hub.src.api_export import keras_hub_export
 from keras_hub.src.tokenizers.byte_pair_tokenizer import BytePairTokenizer
 
+
+@keras.utils.register_keras_serializable(package="keras_hub")
 @keras_hub_export("keras_hub.models.ModernBertTokenizer")
 class ModernBertTokenizer(BytePairTokenizer):
     """ModernBERT tokenizer based on Byte-Pair Encoding.
@@ -19,15 +21,18 @@ class ModernBertTokenizer(BytePairTokenizer):
     def __init__(self, vocabulary=None, merges=None, **kwargs):
         super().__init__(vocabulary=vocabulary, merges=merges, **kwargs)
 
-        # ModernBERT specific special tokens (OLMo vocabulary layout)
-        # These are standard for the answerdotai/ModernBERT-base weights
         self.pad_token_id = 1
         self.bos_token_id = 50279
         self.eos_token_id = 50279
         self.cls_token_id = 50279
         self.sep_token_id = 50279
         self.mask_token_id = 50284
-
+    
+    
     def get_config(self):
         config = super().get_config()
+        config.update({
+            "vocabulary": self.vocabulary,
+            "merges": self.merges,
+        })
         return config
